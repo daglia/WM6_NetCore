5f0f9355fb4b90d441d83bd447d074edf7d938c8
Alper Dağlı <alper379@gmail.com>
2019-03-07 13:52:57 +0300

    Kategori CRUD işlemleri tamamlandı.

--- CoreGiris/Models/Category.cs
+++ CoreGiris/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace CoreGiris.Models
         [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(50),Column("KategoriAdi")]
+        [Required, StringLength(50),Column("KategoriAdi"),DisplayName("Kategori Adı")]
         public string CategoryName { get; set; }
 
         public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
