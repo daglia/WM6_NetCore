15cf0c2ef769c5f36e827a76b19b1d22d597c9ba
Alper Dağlı <alper379@gmail.com>
2019-03-07 11:51:17 +0300

    ASP.NET Core ile veritabanı işlemleri.

---
+++ CoreGiris/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreGiris.Models
+{
+    [Table("Kategoriler")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50),Column("KategoriAdi")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
